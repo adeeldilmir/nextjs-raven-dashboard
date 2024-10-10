7ec880185129a282109fba1c3746d6af4e616d51
Adeel Khan <95750705+adeeldilmir@users.noreply.github.com>
2024-10-11 01:19:33 +0500

    HotFix : Fix linting issues (#26)
    Co-authored-by: Adeel Khan <Adeel.Khan@staples.ca>

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,13 +1,6 @@
-import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import {
-  fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData,
-} from "../../lib/data";
-import { LatestInvoice } from "@/app/lib/definitions";
 import { Suspense } from "react";
 import {
   RevenueChartSkeleton,
